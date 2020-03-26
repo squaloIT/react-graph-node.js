e78413291a026a1190c2a2d32f8cbdda668ffabf
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2020-03-26 15:07:59 +0100

    Added some error handling on products on server

--- server/resources/products.js
+++ server/resources/products.js
@@ -4,22 +4,18 @@ const { pool } = require('./../utils/db');
 const productsAll = async (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
   const jwtDecoded = jwt.decode(token);
-
   console.log(jwtDecoded)
 
-  if (jwtDecoded.id) {
+  if (jwtDecoded && jwtDecoded.id) {
     try {
       pool.query(
         `SELECT p.id, p.product_code, p.description, p.product_name, p.standard_cost, p.list_price, p.category, s.company from products p INNER JOIN suppliers s ON p.supplier_ids = s.id`,
         async function (error, results, fields) {
-          console.log(results)
-
           if (error) throw error;
-
+          console.log("stigao req")
           var products = [
             ...results
           ]
-          console.log(products);
 
           if (!products.length) {
             res.status(204).send();
